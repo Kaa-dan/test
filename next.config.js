937dfe43eb38c8e4f2f2e1056280ad9ff85216a7
kaadan <nithinraj.dev@gmail.com>
2025-02-13 12:28:06 +0530

    chagned

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["techyfi.s3.ap-south-1.amazonaws.com"],
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    domains: ["techyfi.s3.ap-south-1.amazonaws.com"],
+  },
+};
+
+module.exports = nextConfig;
